a6d3992f2cce41fe0af09a288b87a601310e4d09
wael ahmed <w.ahmed020@gmail.com>
2021-05-13 15:50:24 +0100

    connection code

--- employess.js
+++ employess.js
@@ -1,2 +1,12 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+
+    password: 'Winter01',
+    database: 'employee_DB',
+
+})
