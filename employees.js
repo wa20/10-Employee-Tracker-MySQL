70a8f20783617b00f3e9338205adefcca91cde76
wael ahmed <w.ahmed020@gmail.com>
2021-05-14 14:27:08 +0100

    inquiere choices added

--- employees.js
+++ employees.js
@@ -14,8 +14,71 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if(err) throw err;
-    console.log(`connected as id ${connection.threadId}`)
-
+    console.log(`connected as id ${connection.threadId}`);
+    startQuery();
 })
+
+//
+const startQuery = () => {
+inquirer
+    .prompt({
+        type:"list" ,
+        name: 'startChoice' ,
+        message: 'Select an option to begin: ',
+        choices: [
+            'Add Employees',
+            'Add Roles',
+            'Add Department',
+            'View Employee',
+            'View Role',
+            'View Department',
+            'Update Employee',
+            'Update Role',
+            'Update Employee Managers',
+            'Delete Employee',
+            'Delete Role',
+            'Delete Department',
+            'View Total Budget of Department'
+        ]
+
+
+    })
+
+}
+
+// Add employees
+
+// Add roles
+
+// Add department
+
+
+
+
+// View employee
+
+// View role
+
+// View department
+
+
+
+// Update employee
+
+// Upadate role
+
+
+
+//Bonus
+// Update employee managers
+
+// View employee manager
+
+// Delete departments, roles, and employees
+
+// View the total utilized budget of a departemtn -- combined salaries of all emplyees in that department
+
+
+
 
 console.log("hello node, i'm working")
